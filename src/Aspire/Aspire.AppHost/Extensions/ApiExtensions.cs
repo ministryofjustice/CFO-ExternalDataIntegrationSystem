0a09178608b3522d092a011c04342f728f8428e0
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2025-10-30 10:02:24 +0000

    SQL Projects support dotnet and Aspire.
    - Added dotnet support
    - Added packages to aspire to support SQL Projects
    - Configured projects to run at startup.
    
    Not done
    
    - Data seeding.
    - Configuration of other projects to work in aspire

---
+++ src/Aspire/Aspire.AppHost/Extensions/ApiExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Projects;
+
+namespace Aspire.AppHost.Extensions;
+
+public static class ApiExtensions
+{
+    public static IResourceBuilder<ProjectResource> AddDmsApi(
+        this IDistributedApplicationBuilder builder,
+        DmsDatabases databases,
+        IResourceBuilder<ParameterResource> apiKey,
+        IResourceBuilder<ParameterResource> isDevelopment)
+    {
+        return builder.AddProject<API>("api")
+            .WithReference(databases.OfflocRunningPicture)
+            .WithReference(databases.DeliusRunningPicture)
+            .WithReference(databases.Cluster)
+            .WithEnvironment("Authentication__ApiKey", apiKey)
+            .WithEnvironment("IsDevelopment", isDevelopment)
+            .WaitFor(databases.Audit)
+            .WaitFor(databases.OfflocStaging)
+            .WaitFor(databases.DeliusStaging)
+            .WaitFor(databases.OfflocRunningPicture)
+            .WaitFor(databases.DeliusRunningPicture)
+            .WaitFor(databases.Matching);
+    }
+
+    public static IResourceBuilder<ProjectResource> AddDmsVisualiser(
+        this IDistributedApplicationBuilder builder,
+        IResourceBuilder<ProjectResource> apiService,
+        IResourceBuilder<ParameterResource> apiKey)
+    {
+        return builder.AddProject<Visualiser>("visualiser")
+            .WithReference(apiService)
+            .WithEnvironment("API__Key", apiKey)
+            .WaitFor(apiService);
+    }
+}
---
+++ src/Aspire/Aspire.AppHost/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Projects;
+
+namespace Aspire.AppHost.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static IResourceBuilder<SqlServerServerResource> AddDmsDatabase(
+        this IDistributedApplicationBuilder builder,
+        IResourceBuilder<ParameterResource> password)
+    {
+
+#pragma warning disable ASPIREPROXYENDPOINTS001
+        return builder.AddSqlServer("sql", password, 61749)
+            .WithDataVolume("dms-data")
+            .WithLifetime(ContainerLifetime.Persistent)
+            .WithEndpointProxySupport(false)
+            .WithImageTag("2022-latest");
+#pragma warning restore ASPIREPROXYENDPOINTS001
+    }
+
+    public static DmsDatabases AddDmsDatabases(
+        this IDistributedApplicationBuilder builder,
+        IResourceBuilder<SqlServerServerResource> sqlServer)
+    {
+        var audit = sqlServer.AddDatabase("AuditDb");
+        var offlocStaging = sqlServer.AddDatabase("OfflocStagingDb");
+        var deliusStaging = sqlServer.AddDatabase("DeliveryStagingDb");
+        var deliusRunningPicture = sqlServer.AddDatabase("DeliusRunningPictureDb");
+        var offlocRunningPicture = sqlServer.AddDatabase("OfflocRunningPictureDb");
+        var matching = sqlServer.AddDatabase("MatchingDb");
+        var cluster = sqlServer.AddDatabase("ClusterDb");
+
+        builder.AddSqlProject<AuditDb>("audit")
+            .WithReference(audit);
+
+        builder.AddSqlProject<OfflocStagingDb>("OfflocStaging")
+            .WithReference(offlocStaging);
+
+        builder.AddSqlProject<DeliusStagingDb>("DeliusStaging")
+            .WithReference(deliusStaging);
+
+        builder.AddSqlProject<DeliusRunningPictureDb>("DeliusRunningPicture")
+            .WithReference(deliusRunningPicture);
+
+        builder.AddSqlProject<OfflocRunningPictureDb>("OfflocRunningPicture")
+            .WithReference(offlocRunningPicture);
+
+        builder.AddSqlProject<MatchingDb>("Matching")
+            .WithReference(matching)
+            .WithConfigureDacDeployOptions(options => {
+                options.SetVariable("DeliusRunningPictureDb", "DeliusRunningPictureDb");
+                options.SetVariable("OfflocRunningPictureDb", "OfflocRunningPictureDb");
+            });
+
+        builder.AddSqlProject<ClusterDb>("Cluster")
+            .WithReference(cluster)
+            .WithConfigureDacDeployOptions(options => {
+                options.SetVariable("MatchingDb", "MatchingDb");
+                options.SetVariable("DeliusRunningPictureDb", "DeliusRunningPictureDb");
+                options.SetVariable("OfflocRunningPictureDb", "OfflocRunningPictureDb");
+            });
+
+        return new DmsDatabases(
+            audit,
+            offlocStaging,
+            deliusStaging,
+            deliusRunningPicture,
+            offlocRunningPicture,
+            matching,
+            cluster
+        );
+    }
+}
+
+public record DmsDatabases(
+    IResourceBuilder<SqlServerDatabaseResource> Audit,
+    IResourceBuilder<SqlServerDatabaseResource> OfflocStaging,
+    IResourceBuilder<SqlServerDatabaseResource> DeliusStaging,
+    IResourceBuilder<SqlServerDatabaseResource> DeliusRunningPicture,
+    IResourceBuilder<SqlServerDatabaseResource> OfflocRunningPicture,
+    IResourceBuilder<SqlServerDatabaseResource> Matching,
+    IResourceBuilder<SqlServerDatabaseResource> Cluster
+);
