d5a749caf2f89f584679c21b99dee943835a6eab
samgibsonmoj <sam.gibson@justice.gov.uk>
2025-10-31 13:59:07 +0000

    Adds Aspire infrastructure for DMS

--- src/Aspire/Aspire.AppHost/Extensions/AppExtensions.cs
+++ src/Aspire/Aspire.AppHost/Extensions/AppExtensions.cs
@@ -6,32 +6,74 @@ public static class AppExtensions
 {
     public static IResourceBuilder<ProjectResource> AddDmsApi(
         this IDistributedApplicationBuilder builder,
-        DmsDatabases databases,
+        DmsDatabaseResources databases,
         IResourceBuilder<ParameterResource> apiKey,
         IResourceBuilder<ParameterResource> isDevelopment)
     {
         return builder.AddProject<API>("api")
-            .WithReference(databases.OfflocRunningPicture)
-            .WithReference(databases.DeliusRunningPicture)
-            .WithReference(databases.Cluster)
+            .WithDmsDatabaseReference(databases.OfflocRunningPicture)
+            .WithDmsDatabaseReference(databases.DeliusRunningPicture)
+            .WithDmsDatabaseReference(databases.Cluster)
+            .WithDmsDatabaseReference(databases.Audit)
             .WithEnvironment("Authentication__ApiKey", apiKey)
-            .WithEnvironment("IsDevelopment", isDevelopment)
-            .WaitFor(databases.Audit)
-            .WaitFor(databases.OfflocStaging)
-            .WaitFor(databases.DeliusStaging)
-            .WaitFor(databases.OfflocRunningPicture)
-            .WaitFor(databases.DeliusRunningPicture)
-            .WaitFor(databases.Matching);
+            .WithEnvironment("IsDevelopment", isDevelopment);
     }
 
     public static IResourceBuilder<ProjectResource> AddDmsVisualiser(
         this IDistributedApplicationBuilder builder,
-        IResourceBuilder<ProjectResource> apiService,
-        IResourceBuilder<ParameterResource> apiKey)
+        IResourceBuilder<ProjectResource> apiService)
     {
         return builder.AddProject<Visualiser>("visualiser")
-            .WithReference(apiService)
-            .WithEnvironment("API__Key", apiKey)
-            .WaitFor(apiService);
+            .WithReference(apiService).WaitFor(apiService);
     }
+
+    public static IDistributedApplicationBuilder AddDmsServices(
+        this IDistributedApplicationBuilder builder,
+        IResourceBuilder<RabbitMQServerResource> rabbit,
+        DmsDatabaseResources databases)
+    {
+        builder.AddDmsService<Blocking>("Blocking", rabbit, databases);
+        builder.AddDmsService<Cleanup>("Cleanup", rabbit, databases);
+        builder.AddDmsService<DbInteractions>("DbInteractions", rabbit, databases);
+        builder.AddDmsService<Delius_Parser>("Delius-Parser", rabbit, databases);
+        builder.AddDmsService<Import>("Import", rabbit, databases);
+        builder.AddDmsService<Logging>("Logging", rabbit, databases);
+        builder.AddDmsService<Matching_Engine>("Matching-Engine", rabbit, databases);
+        //builder.AddDmsService<Meow>("Meow", rabbit, databases);
+        builder.AddDmsService<Offloc_Cleaner>("Offloc-Cleaner", rabbit, databases);
+        builder.AddDmsService<Offloc_Parser>("Offloc-Parser", rabbit, databases);
+        builder.AddDmsService<Orchestrator>("Orchestrator", rabbit, databases);
+
+        // Entry point - start the app to 'kick off' DMS
+        builder.AddDmsService<Kickoff>("Kickoff", rabbit, databases)
+            .WithExplicitStart();
+
+        return builder;
+    }
+
+    private static IResourceBuilder<ProjectResource> AddDmsService<TDmsProject>(
+        this IDistributedApplicationBuilder builder, 
+        string name,
+        IResourceBuilder<RabbitMQServerResource> rabbit,
+        DmsDatabaseResources databases) where TDmsProject : IProjectMetadata, new()
+    {
+        var project = builder.AddProject<TDmsProject>(name)
+            .WithReference(rabbit).WaitFor(rabbit)
+            .WithDmsDatabaseReference(databases.OfflocStaging)
+            .WithDmsDatabaseReference(databases.OfflocRunningPicture)
+            .WithDmsDatabaseReference(databases.DeliusStaging)
+            .WithDmsDatabaseReference(databases.DeliusRunningPicture)
+            .WithDmsDatabaseReference(databases.Audit)
+            .WithDmsDatabaseReference(databases.Cluster)
+            .WithDmsDatabaseReference(databases.Matching);
+
+        return project;
+    }
+
+    private static IResourceBuilder<ProjectResource> WithDmsDatabaseReference(this IResourceBuilder<ProjectResource> builder, DmsDatabaseResource database)
+    {
+        builder.WithReference(database.DatabaseResource).WaitForCompletion(database.SqlProjectResource);
+        return builder;
+    }
+
 }
