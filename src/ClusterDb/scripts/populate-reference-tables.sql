5a98035fdbbd18457281d24b961e3d6b046535a1
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2025-11-04 18:29:50 +0000

    Develop (#9)
    * Publish build artifacts
    
    * Deploy on push to main
    
    * sqlproj required field
    
    * Finalise save network
    
    Includes some UX improvements to prevent double-clicking save/cancel etc
    
    * Remove Orchestrator from Aspire orchestration (unneeded)
    
    * Include status message for post-deployment ClusterDb script
    
    * Prefer raiserror over print for status messages

--- src/ClusterDb/scripts/populate-reference-tables.sql
+++ src/ClusterDb/scripts/populate-reference-tables.sql
@@ -1,4 +1,5 @@
 IF '$(PopulateReferenceTables)' = 'True'
 BEGIN
+    RAISERROR (N'Executing post deployment script. This may take a while...', 0, 0) WITH NOWAIT;
     EXEC processing.PopulateReferenceTables;
 END
