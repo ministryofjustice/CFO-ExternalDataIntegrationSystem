6ca28963928e4094e95158399a2072784b832d65
Sam Gibson <140488216+samgibsonmoj@users.noreply.github.com>
2025-11-25 10:59:20 +0000

    Upgrade aspire (#20)
    * Updates Aspire dependencies to v13
    
    * Configures parameters for various services
    
    * Updates CommunityToolkit packages to stable version

--- src/Aspire/Aspire.AppHost/Program.cs
+++ src/Aspire/Aspire.AppHost/Program.cs
@@ -3,15 +3,17 @@ using Aspire.AppHost.Extensions;
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Parameters
-var password = builder.AddParameter("password", true);
-var apiKey = builder.AddParameter("apikey", true);
-var isDevelopment = builder.AddParameter("IsDevelopment");
+var apiKey = builder.AddParameter("apiKey", true);
+var isDevelopment = builder.AddParameter("isDevelopment");
+var rabbitPassword = builder.AddParameter("rabbitPassword", true);
+var sqlPassword = builder.AddParameter("sqlPassword", true);
+var minioPassword = builder.AddParameter("minioPassword", true);
 
 var hostMount = HostExtensions.Create(Path.Combine(builder.AppHostDirectory, "DMS_STAGING"));
 var targetMount = "/app/";
 
 // Database setup
-var sql = builder.AddDmsSqlServer(password)
+var sql = builder.AddDmsSqlServer(sqlPassword)
     .WithBindMount(hostMount, targetMount);
 
 var databases = builder.AddDmsDatabases(sql, seedData: false);
@@ -23,11 +25,11 @@ var apiService = builder.AddDmsApi(databases, apiKey, isDevelopment);
 builder.AddDmsVisualiser(apiService);
 
 var rabbit = builder
-    .AddRabbitMQ("RabbitMQ")
+    .AddRabbitMQ("RabbitMQ", password: rabbitPassword)
     .WithManagementPlugin();
 
 // MinIO (s3 emulation)
-var minio = builder.AddMinioContainer("minio")
+var minio = builder.AddMinioContainer("minio", rootPassword: minioPassword)
     .WithDataVolume("dms-minio-data");
 
 builder.AddDmsServices(
