0fef639eaac987821aa41b4238871336d4979f4c
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-01-30 11:31:41 +0000

    Adds recent processed files endpoint (#84)
    Exposes an endpoint to retrieve and display recently processed files from both Offloc and Delius contexts.
    
    This provides users with a quick overview of the latest file processing activity.

--- src/Libraries/Infrastructure/Entities/Delius/ProcessedFile.cs
+++ src/Libraries/Infrastructure/Entities/Delius/ProcessedFile.cs
@@ -1,8 +1,14 @@
-﻿namespace Infrastructure.Entities.Delius;
+﻿using Microsoft.Identity.Client;
+
+namespace Infrastructure.Entities.Delius;
 
 public partial class ProcessedFile
 {
     public string FileName { get; set; } = null!;
 
     public int FileId { get; set; }
+
+    public string Status {get; set;} = null!;
+
+    public DateTime ValidFrom { get;set; }
 }
--- src/Libraries/Infrastructure/Entities/Offloc/ProcessedFile.cs
+++ src/Libraries/Infrastructure/Entities/Offloc/ProcessedFile.cs
@@ -5,4 +5,8 @@ public partial class ProcessedFile
     public string FileName { get; set; } = null!;
 
     public int FileId { get; set; }
+
+    public DateTime ValidFrom {get;set;}
+
+    public string Status{ get;set;} = null!;
 }
