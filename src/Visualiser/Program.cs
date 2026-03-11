9c75c238becde6c6651b55f08b4254e1bffed5e0
Carl Sixsmith <carl.sixsmith1@justice.gov.uk>
2026-03-11 18:04:05 +0000

    Implement security hardening with CSP and localized assets
    Implements a Content Security Policy (CSP) and localizes the vis-network dependency to ensure compliance. Additionally, this adds anti-forgery token validation to page models and forces the HTTPS scheme in middleware.

--- src/Visualiser/Program.cs
+++ src/Visualiser/Program.cs
@@ -59,6 +59,23 @@ app.UseAuthorization();
 app.UseRequestTimeouts();
 app.UseOutputCache();
 
+app.Use((context, next) =>
+{
+   context.Request.Scheme = "https";
+   return next(); 
+});
+
+app.Use(async (context, next) =>
+{
+    if(context.Response.Headers.IsReadOnly == false)
+    {
+        string csp = app.Configuration["Content-Security-Policy"]
+                            ??  "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; object-src 'self' data:; frame-src 'self' data:;";   
+        context.Response.Headers.Append("Content-Security-Policy", csp);
+    }
+    await next();
+});
+
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
