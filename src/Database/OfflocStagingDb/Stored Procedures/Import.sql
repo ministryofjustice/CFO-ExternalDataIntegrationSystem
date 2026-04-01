7302c06111041c34dab382fe0142e661e83db4a4
Ryan Kearsley <ryan.kearsley@justice.gov.uk>
2026-04-01 18:22:23 +0100

    Refactor: Update Offloc staging procedure to use SqlBulkCopy for file imports due to BULK INSERT not working with containerisation
    Update: Altered FileSync ProcessOnStartup to true

--- src/Database/OfflocStagingDb/Stored Procedures/Import.sql
+++ src/Database/OfflocStagingDb/Stored Procedures/Import.sql
@@ -2,75 +2,23 @@
 -- Author:		Daniel Fenelon
 -- Create date: 18/03/2024
 -- Description:	Import of the OffLoc files
+-- Note:        File loading removed - data is now staged via SqlBulkCopy
+--              in the DbInteractions service. This procedure handles
+--              post-load standardisation and status update only.
 -- =============================================
 
 CREATE PROCEDURE [OfflocStaging].[Import]
-    @basePath VARCHAR(100),
     @processedFile VARCHAR(50)
 AS
 BEGIN
 
     SET NOCOUNT ON;
     SET DATEFORMAT DMY;
-    
-    CREATE TABLE #TableNames
-    (
-        Id INT PRIMARY KEY IDENTITY,
-        fileName VARCHAR(50) NOT NULL
-    );
-    INSERT INTO #TableNames
-    (
-        fileName
-    )
-    VALUES
-    ('Activities'),
-    ('Addresses'),
-    ('Agencies'),
-    ('Assessments'),
-    ('Bookings'),
-    ('Employment'),
-    ('Flags'),
-    ('Identifiers'),
-    ('IncentiveLevel'),
-    ('Locations'),
-    ('MainOffence'),
-    ('Movements'),
-    ('OffenderAgencies'),
-    ('OffenderStatus'),
-    ('OtherOffences'),
-    ('PersonalDetails'),
-    ('PNC'),
-    ('PreviousPrisonNumbers'),
-    ('SentenceInformation'),
-    ('SexOffenders');
-
-    DECLARE @i INT;
-    DECLARE @sql VARCHAR(250);
-    DECLARE @fileName VARCHAR(150);
-
-    SET @i = 1;
 
     BEGIN TRANSACTION;
     BEGIN TRY
-        WHILE @i <= (SELECT COUNT(*)FROM #TableNames)
-        BEGIN
-            SET @fileName =
-            (
-                SELECT fileName FROM #TableNames WHERE Id = @i
-            );
-            SET @sql
-                = N'BULK INSERT OfflocStaging.' + @fileName + N' FROM ''' + @basePath + @fileName + '.txt'''
-                  + N' WITH (FieldTerminator = ''|'', RowTerminator = ''0x0d0a'', MAXERRORS = 1000)';
-            EXEC (@sql);
-
-            SET @i = @i + 1;
-        END;
-        DECLARE @stringDate CHAR(8);
-        SET @stringDate = SUBSTRING(@processedFile, 18, 8);
-
-        --Standardise Offloc Data
-        Declare @retMessage varchar(500);
-        EXEC [OfflocStaging].[StandardiseData] @retMessage;
+        DECLARE @retMessage VARCHAR(500);
+        EXEC [OfflocStaging].[StandardiseData] @retMessage OUTPUT;
 
         UPDATE [OfflocRunningPictureDb].[OfflocRunningPicture].[ProcessedFiles]
         SET [Status] = 'Imported'
