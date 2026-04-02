7e40395fdc222813af4fe3d11002c263f58c8a50
Ryan Kearsley <ryan.kearsley@justice.gov.uk>
2026-04-02 12:42:40 +0100

    Testig: Change Delius to also use SqlBulkCopy like Offloc does

--- src/Database/DeliusStagingDb/Stored Procedures/StageDelius.sql
+++ src/Database/DeliusStagingDb/Stored Procedures/StageDelius.sql
@@ -1,47 +1,19 @@
 ﻿CREATE PROCEDURE [DeliusStaging].[StageDelius]
-	@basePath VARCHAR(100), -- Should be in the format /app/delius/{fileName}.
-	@processedFile VARCHAR(50),
-	@inContainer VARCHAR(1)
+	@processedFile VARCHAR(50)
 AS
-	IF @inContainer = 'N'
-	BEGIN 
-		SET NOCOUNT ON; 
-		SET DATEFORMAT DMY;
-	END;
+BEGIN
+	SET NOCOUNT ON;
+	SET DATEFORMAT DMY;
 
-	CREATE TABLE #TableNames(Id int Primary Key Identity, fileName VARCHAR(50));
-	INSERT INTO #TableNames(fileName) VALUES
-		('AdditionalIdentifier'), ('AliasDetails'), ('Disability'), ('Disposal'), ('EventDetails'), ('Header'), ('MainOffence'), 
-		('OAS'), ('OffenderAddress'), ('OffenderManager'), ('OffenderManagerBuildings'), ('OffenderManagerTeam'), 
-		('OffenderToOffenderManagerMappings'), ('Offenders'), ('OffenderTransfer'), ('PersonalCircumstances'), 
-		('Provision'), ('RegistrationDetails'), ('Requirement');
-	
-	DECLARE @i int;
-	DECLARE @sql NVARCHAR(250);
-	DECLARE @fileName VARCHAR(150);
-
-	SET @i = 1;
-
-	BEGIN TRANSACTION;	
+	BEGIN TRANSACTION;
 	BEGIN TRY
-		WHILE @i <= (Select COUNT(*) FROM #TableNames)
-		BEGIN
-			SET @fileName = (SELECT fileName FROM #TableNames WHERE Id = @i);
-			SET @sql = 'BULK INSERT DeliusStaging.' + @fileName + ' FROM ''' + @basePath + @fileName + '.txt''' +
-			' WITH (FieldTerminator=''|'', RowTerminator = ''0x0d0a'', MAXERRORS = 1000)';
-			
-			EXEC (@sql);
-			SET @i = @i+1;
-		END
-
-		--Standardise Delius Data
-        Declare @retMessage varchar(500);
-		EXEC [DeliusStaging].[StandardiseData] @retMessage;
+		DECLARE @retMessage VARCHAR(500);
+		EXEC [DeliusStaging].[StandardiseData] @retMessage OUTPUT;
 
 		UPDATE [DeliusRunningPictureDb].[DeliusRunningPicture].[ProcessedFiles]
 		SET [Status] = 'Imported'
 		WHERE FileName = @processedFile;
-		
+
 		COMMIT TRANSACTION;
 	END TRY
 	BEGIN CATCH
